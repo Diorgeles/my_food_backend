327ac00a6f18075513157a90bc977f15dae3baae
Diorgeles Dias Lima <diorgelesdias@gmail.com>
2019-08-25 15:53:46 -0300

    fix docker-compose

--- core/models.py
+++ core/models.py
@@ -8,16 +8,37 @@ class Recipe(SoftDeletableModel, TimeStampedModel):
 
     # TODO: Define fields here
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    subscription = models.CharField(max_length=8, editable=False)
-    name = models.CharField('Nome', max_length=100)
+    name = models.CharField('Nome', max_length=100, null=True, blank=True)
+    description = models.TextField('Descrição', max_length=255, null=True, blank=True)
+    ingredients = models.ManyToManyField('Ingredientes', max_length=8, editable=False)
 
     class Meta:
         """Meta definition for Students."""
 
-        verbose_name = 'Estudante'
-        verbose_name_plural = 'Estudantes'
+        verbose_name = 'Receita'
+        verbose_name_plural = 'Receitas'
 
     def __str__(self):
         """Unicode representation of Students."""
         return self.name
+
+class Ingredients(SoftDeletableModel, TimeStampedModel):
+
+    MEASURE_TYPE = [
+        ('spoon', 'Colher'),
+        ('cup', 'Xícara'),
+    ]
+
+    name = models.CharField('Nome', max_length=100, null=True, blank=True)
+    amount = models.IntegerField('Quatidade', max_length=100, null=True, blank=True)
+    measure = models.CharField('Medida', max_length=100, null=True, blank=True, choices=MEASURE_TYPE)
+
+    class Meta:
+        verbose_name = 'Ingrediente'
+        verbose_name_plural = 'Ingredientes'
+
+    def __str__(self):
+        """Unicode representation of Students."""
+        return self.name
+
 
--- my_food_backend/settings.py
+++ my_food_backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'my_food_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# HEROKU #
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'core_home'
